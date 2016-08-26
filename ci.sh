9e64f5b5bb89e7d3489b3f3d0ef4958bd6eed2ce
Tyler Baker <tyler.baker@linaro.org>
2016-08-26 13:24:09 -0700

    Remove /dev/usb mappings for better portability

--- ci.sh
+++ ci.sh
@@ -20,7 +20,7 @@ fi
 
 sudo docker build -t lava lava-docker/
 sudo docker build -t zephyr-builder ubuntu/
-sudo docker run -d -t -v /dev/bus/usb:/dev/bus/usb -v /dev/serial:/dev/serial -v /boot:/boot -v /lib/modules:/lib/modules -p 80:80 -p 2022:22 -h lava-docker --name lava --privileged lava
+sudo docker run -d -t -v /dev:/dev/ -v /lib/modules:/lib/modules -p 80:80 -p 2022:22 -h lava-docker --name lava --privileged lava
 sudo docker run -it -e ZEPHYR_OUTDIR="/var/www/html/zephyr" -p 8000:8000 -h zephyr-ci --link lava zephyr-builder
 sudo docker kill lava
 sudo docker rm -f lava
