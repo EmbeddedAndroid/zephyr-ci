941c36eae3ddef006348b0434c7969fe6dd4551b
Tyler Baker <tyler.baker@linaro.org>
2016-08-26 09:48:42 -0700

    Update docker repos if they exist

--- ci.sh
+++ ci.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-git clone https://github.com/EmbeddedAndroid/lava-docker.git -b las16-demo
-git clone https://github.com/EmbeddedAndroid/ubuntu.git -b las16-dev
+if [ -d "lava-docker" ]; then
+	cd lava-docker
+	git pull
+	cd ..
+else
+	git clone https://github.com/EmbeddedAndroid/lava-docker.git -b las16-demo
+fi
+
+if [ -d "ubuntu" ]; then
+	cd ubuntu
+	git pull
+	cd ..
+else
+	git clone https://github.com/EmbeddedAndroid/ubuntu.git -b las16-dev
+fi
+
 sudo docker build -t lava lava-docker/
 sudo docker build -t zephyr-builder ubuntu/
 sudo docker run -d -t -v /dev/bus/usb:/dev/bus/usb -v /dev/serial:/dev/serial -v /boot:/boot -v /lib/modules:/lib/modules -v /home/tyler/Dev/docker/lava/fileshare:/opt -p 80:80 -p 2022:22 -h lava-docker --name lava --privileged lava
